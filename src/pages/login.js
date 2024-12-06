f73fec4621f4faa1c3ef5b57b6aab4ada323a712
Marek Harrison <marekdharrison@gmail.com>
2024-12-06 16:34:44 +0000

    added useState hooks to obtain data from forms

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/login.css'
 function Login() {
+
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		alert(`The password you entered was: ${password}`)
+	}
 
 	return (
 		<div className="container">
@@ -9,12 +18,12 @@ function Login() {
 				<h1>Welcome Back</h1>
 			</div>
 			<div className="body">
-				<form>
+				<form onSubmit={handleSubmit}>
 					<label>E-Mail: <br />
-						<input type="email" /> <br /> <br />
+						<input type="email" value={email} onChange={(e) => setEmail(e.target.value)} /> <br /> <br />
 					</label>
 					<label>Password: <br />
-						<input type="text" /> <br /> <br />
+						<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} /> <br /> <br />
 					</label>
 					<Link to='/home'>
 						<button type="submit">Log In</button>
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,4 +1,16 @@
+import React from 'react';
+import { useState } from 'react';
 function Register() {
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [phone, setPhone] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`The password you entered was: ${password}`)
+    }
 
     return (
 
@@ -7,18 +19,18 @@ function Register() {
                 <h1>Create Account</h1>
             </div>
             <div className="body">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label>Name: <br />
-                        <input type="text" /> <br /> <br />
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} /> <br /> <br />
                     </label>
                     <label>E-Mail Address: <br />
-                        <input type="email" /> <br /> <br />
+                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} /> <br /> <br />
                     </label>
                     <label>Phone Number: <br />
-                        <input type="tel" /> <br /> <br />
+                        <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} /> <br /> <br />
                     </label>
                     <label>Password: <br />
-                        <input type="password" /> <br /> <br />
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} /> <br /> <br />
                     </label>
                     <button type="submit">Register</button>
                 </form>
